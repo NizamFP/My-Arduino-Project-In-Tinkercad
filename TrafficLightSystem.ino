9464b182b67650443269a97e8e575d9a0c510bcb
NizamFP <71487616+NizamFP@users.noreply.github.com>
2022-01-26 00:03:04 +0700

    Create TrafficLightSystem.ino

---
+++ TrafficLightSystem.ino
@@ -0,0 +1,265 @@
+// C++ code
+// This is The Project Link: https://www.tinkercad.com/things/lxTTcUrtxtx
+int animationSpeed = 0;
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  if (2 < 1) {
+    // Number0
+    digitalWrite(2, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(3, HIGH);
+  }
+
+  digitalWrite(13, HIGH);
+  // Number5
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number5Off
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  // Number4
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number4Off
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(2, LOW);
+  // Number3
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number3Off
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  // Number2
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number2Off
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  // Number1
+  digitalWrite(5, HIGH);
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number1Off
+  digitalWrite(5, LOW);
+  digitalWrite(2, LOW);
+  // Number0
+  digitalWrite(2, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  delay(100); // Wait for 100 millisecond(s)
+  // Number0Off
+  digitalWrite(2, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  delay(100); // Wait for 100 millisecond(s)
+  // Number1
+  digitalWrite(5, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number1Off
+  digitalWrite(5, LOW);
+  digitalWrite(2, LOW);
+  // Number0
+  digitalWrite(2, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(12, LOW);
+  delay(100); // Wait for 100 millisecond(s)
+  // Number0Off
+  digitalWrite(2, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(11, HIGH);
+  // Number5
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number5Off
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  // Number4
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number4Off
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(2, LOW);
+  // Number3
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number3Off
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  // Number2
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number2Off
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  // Number1
+  digitalWrite(5, HIGH);
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // Number1Off
+  digitalWrite(5, LOW);
+  digitalWrite(2, LOW);
+  // Number0
+  digitalWrite(2, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  delay(100); // Wait for 100 millisecond(s)
+  // Number0Off
+  digitalWrite(2, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(11, LOW);
+
+  if (2 < 1) {
+    // Number5
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+  }
+
+  if (2 < 1) {
+    // Number2
+    digitalWrite(6, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(3, HIGH);
+  }
+
+  if (2 < 1) {
+    // Number1
+    digitalWrite(5, HIGH);
+    digitalWrite(2, HIGH);
+  }
+
+  if (2 < 1) {
+    // Number3
+    digitalWrite(6, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+  }
+
+  if (2 < 1) {
+  }
+
+  if (2 < 1) {
+    // Number4
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(2, HIGH);
+  }
+
+  if (2 < 1) {
+    // Number6
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+
+  if (2 < 1) {
+    // Number7
+    digitalWrite(6, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(2, HIGH);
+  }
+}
